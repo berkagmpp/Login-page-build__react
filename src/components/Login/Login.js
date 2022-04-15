4449179081d67fe187790fbe4e94f15f75aed41f
Claire <berkagmpp@gmail.com>
2022-04-15 15:20:52 +1200

    lean App component with useContext (add AuthContextProvider in auth-context.js)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
+
+import AuthContext from '../store/auth-context';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -34,6 +36,8 @@ const Login = (props) => {
     const [emailState, dispatchEmail] = useReducer(emailReducer,{ value: '', isValid: undefined });
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: undefined });
     
+    const authCtx = useContext(AuthContext);
+
     const { isValid: emailIsValid } = emailState;
     const { isValid: passwordIsValid } = passwordState;
 
@@ -81,7 +85,7 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
+import { AuthContextProvider } from './components/store/auth-context';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
+);
